5557f96032dc511678eb0cc9b1cf78218e5997d4
Torbjørn Thorsen <torbjorn@trollweb.no>
2011-08-11 22:42:04 +0200

    State and state handling for Game

--- games/models.py
+++ games/models.py
@@ -1,13 +1,56 @@
 from django.db import models
 
+from django.core.exceptions import ValidationError
 
 from courses.models import Course, CourseHole
 from players.models import Player
 
 
 class Game(models.Model):
+    STATE_CREATED = 1
+    STATE_STARTED = 2
+    STATE_FINISHED = 3
+    STATE_ABORTED = 4
+
+    STATE_CHOICES = (
+        (STATE_CREATED, "Created"),
+        (STATE_STARTED, "Started"),
+        (STATE_FINISHED, "Finished"),
+        (STATE_ABORTED, "Aborted"),
+    )
+
     course = models.ForeignKey(Course)
     players = models.ManyToManyField(Player)
+    state = models.PositiveSmallIntegerField(
+        choices=STATE_CHOICES, default=STATE_CREATED,
+    )
+
+    def start(self):
+        if self.state is not self.STATE_CREATED:
+            # TODO: This is bad, we should at least get the state
+            # description from STATE_CHOICES
+            raise ValidationError("Can only start game when state is '%s'"
+                % (self.STATE_CREATED))
+
+        self.state = self.STATE_STARTED
+
+    def finish(self):
+        if self.state is not self.STATE_STARTED:
+            # TODO: This is bad, we should at least get the state
+            # description from STATE_CHOICES
+            raise ValidationError("Can only finish game when state is '%s'"
+                % (self.STATE_STARTED))
+
+        self.state = self.STATE_FINISHED
+
+    def abort(self):
+        if self.state is not self.STATE_STARTED:
+            # TODO: This is bad, we should at least get the state
+            # description from STATE_CHOICES
+            raise ValidationError("Can only abort game when state is '%s'"
+                % (self.STATE_STARTED))
+
+        self.state = self.STATE_ABORTED
 
 
 class GameHole(models.Model):
--- games/tests.py
+++ games/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+from django.core.exceptions import ValidationError
 
 from games.models import Game
 
@@ -7,15 +8,76 @@ from courses.tests import make_arenas, make_course
 from players.tests import make_players
 
 
+def make_game():
+    arena = make_arenas()[0]
+    course = make_course(arena)[0]
+    players = make_players(5)
+
+    game = Game.objects.create(
+        course=course,
+    )
+    game.players = players
+
+    return game
+
+
 class GamesTest(TestCase):
     def test_basic(self):
-        arena = make_arenas()[0]
-        course = make_course(arena)[0]
-        players = make_players(5)
-
-        game = Game.objects.create(
-            course=course,
-        )
-        game.players = players
-
+        game = make_game()
         self.assertNotEqual(game.id, None)
+
+    def test_game_start(self):
+        game = make_game()
+
+        # Game starts out created
+        self.assertEqual(game.state, game.STATE_CREATED)
+
+        # Start game
+        game.start()
+        game.save()
+
+        # Check that game is started
+        self.assertEqual(game.state, game.STATE_STARTED)
+
+        # Check that game can not be started again
+        self.assertRaises(ValidationError, game.start)
+
+    def test_game_finish(self):
+        game = make_game()
+        game.start()
+        game.save()
+
+        # Check that game is started
+        self.assertEqual(game.state, game.STATE_STARTED)
+
+        game.finish()
+        game.save()
+
+        # Check that game is finished
+        self.assertEqual(game.state, game.STATE_FINISHED)
+
+        # Check that game can not be finished again
+        self.assertRaises(ValidationError, game.finish)
+
+        # Check that game can not be started again
+        self.assertRaises(ValidationError, game.start)
+
+    def test_game_abort(self):
+        game = make_game()
+        game.start()
+        game.save()
+
+        # Check that game is started
+        self.assertEqual(game.state, game.STATE_STARTED)
+
+        game.abort()
+        game.save()
+
+        # Check that game is aborted
+        self.assertEqual(game.state, game.STATE_ABORTED)
+
+        # Check that game can not be aborted again
+        self.assertRaises(ValidationError, game.abort)
+
+        # Check that game can not be started again
+        self.assertRaises(ValidationError, game.start)
