af13edc0ac22ab27b86bbaa8ad02c0f3ecfad402
Jone Eide <jone@idev.no>
2011-09-12 16:31:00 +0200

    Create and list for arenas

---
+++ courses/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls.defaults import patterns, url
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import (ListView,
+                                  DetailView,
+                                  CreateView,
+                                  DeleteView,
+                                  UpdateView,)
+
+from courses.models import Arena
+
+urlpatterns = patterns('',
+    url(r'^courses/$', ListView.as_view(
+        model=Arena,
+        context_object_name="arenas",
+    ), name="golfstats-courses-arenas"),
+
+    url(r'^courses/create/$', CreateView.as_view(
+        model=Arena,
+        success_url=reverse_lazy('golfstats-courses-arenas'),
+    ), name="golfstats-courses-arenas-create"),
+)
--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,12 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls.defaults import patterns, url, include
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(
         template_name="index.html",
     ), name="golfstats-index"),
+)
+
+urlpatterns += patterns('',
+    url(r'', include('courses.urls')),
 )
