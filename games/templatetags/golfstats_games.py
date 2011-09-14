fc278928b6ec5d4cb6e2df07235eea9c2fd9db04
Jone Eide <jone@idev.no>
2011-09-15 01:31:31 +0200

    Templatetag for generating game standing tables

--- games/templatetags/golfstats_games.py
+++ games/templatetags/golfstats_games.py
@@ -5,6 +5,37 @@ register = template.base.Library()
 
 
 @register.tag
+def game_standings_table(parser, token):
+
+    try:
+        tag_name, game = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError(
+            "%r tag requires one arguments" %
+            token.contents.split()[0])
+
+    return GameStandingsNode(game)
+
+
+class GameStandingsNode(template.Node):
+    def __init__(self, game):
+        self.game = Variable(game)
+
+    def render(self, context):
+        super(GameStandingsNode, self).render(context)
+        game = self.game.resolve(context)
+
+        t = template.loader.get_template(
+            'templatetags/game_standings_table.html')
+
+        t_context = template.Context({
+            'game': game,
+        })
+
+        return t.render(t_context)
+
+
+@register.tag
 def get_hole_score_css_class(parser, token):
 
     try:
