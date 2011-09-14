f84ca71ea51d743ce090c9426a12d0b4757ce2e3
Jone Eide <jone@idev.no>
2011-09-15 00:26:59 +0200

    Changed templatetag to return GameHole

--- games/templatetags/score_game_player_hole.py
+++ games/templatetags/score_game_player_hole.py
@@ -10,20 +10,23 @@ def get_player_score(parser, token):
 
     try:
         tag_name, player_id, \
-            coursehole_id, game_id = token.split_contents()
+            coursehole_id, game_id, \
+            return_var = token.split_contents()
     except ValueError:
         raise template.TemplateSyntaxError(
             "%r tag requires three arguments" %
             token.contents.split()[0])
 
-    return PlayerScoreNode(player_id, coursehole_id, game_id)
+    return PlayerScoreNode(player_id, coursehole_id,
+        game_id, return_var)
 
 
 class PlayerScoreNode(template.Node):
-    def __init__(self, player_id, coursehole_id, game_id):
+    def __init__(self, player_id, coursehole_id, game_id, return_var):
         self.player_id = Variable(player_id)
         self.coursehole_id = Variable(coursehole_id)
         self.game_id = Variable(game_id)
+        self.return_var = return_var
 
     def render(self, context):
         super(PlayerScoreNode, self).render(context)
@@ -35,6 +38,8 @@ class PlayerScoreNode(template.Node):
                 player__id=self.player_id.resolve(context),
             )
         except GameHole.DoesNotExist:
-            return 0
+            context[self.return_var] = 0
         else:
-            return gamehole.throws
+            context[self.return_var] = gamehole
+
+        return ''
