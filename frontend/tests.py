2515797ed13d9d25a3ff230652ced9bab745536f
Torbjørn Thorsen <torbjorn@trollweb.no>
2011-10-16 04:21:14 +0200

    Disable test for frontend debug views
    Seems that test depends on the order of tests, so disabled for now.

--- frontend/tests.py
+++ frontend/tests.py
@@ -1,6 +1,7 @@
-from django.conf import settings
 from django.test import TestCase, Client
 from nose.plugins.attrib import attr  # NOQA
+
+#from django.conf import settings  # NOQA
 
 
 class FrontendTest(TestCase):
@@ -11,20 +12,24 @@ class FrontendTest(TestCase):
         self.assertContains(r, "Welcome to Golfstats", count=1)
 
 
-class FrontendDebugTest(TestCase):
-    def test_login(self):
-        # TODO: Hmf, does this work as expected ?
-        old_debug = settings.DEBUG
-        settings.DEBUG = True
-
-        c = Client()
-        r = c.get("/debug/login/")
-
-        # SessionID cookie has been set ?
-        self.assertIn("sessionid", r.cookies.keys())
-
-        # We have been redirected
-        self.assertEqual(r.status_code, 302)
-
-        # Restore settings.DEBUG
-        settings.DEBUG = old_debug
+#class FrontendDebugTest(TestCase):
+#    # TODO: It seems this test only works if it's the first test.
+#    # I belive that has something to do with urls.py checking for
+#    # settings.DEBUG before including the URL routing for this view.
+#    @attr("smalltest")
+#    def test_login(self):
+#        # TODO: Hmf, does this work as expected ?
+#        old_debug = settings.DEBUG
+#        settings.DEBUG = True
+#
+#        c = Client()
+#        r = c.get("/debug/login/")
+#
+#        # SessionID cookie has been set ?
+#        self.assertIn("sessionid", r.cookies.keys())
+#
+#        # We have been redirected
+#        self.assertEqual(r.status_code, 302)
+#
+#        # Restore settings.DEBUG
+#        settings.DEBUG = old_debug
